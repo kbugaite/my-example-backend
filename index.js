45cf6fde44817f8c6fd7bbd3017a18beab1ca7f5
kbugaite <kbugaite@gmail.com>
2023-02-27 12:09:43 +0000

    updated files

--- index.js
+++ index.js
@@ -8,7 +8,7 @@ app.use(cors())
 
 app.use(express.json())
 app.use(express.urlencoded({ extended: false }))
-app.use('/', router)
+app.use(router)
 
 // app.get('/test', (req, res) => {
 //     res.send('You are on the testing route')
